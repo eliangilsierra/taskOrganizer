1745185c6ab62e6b768e024cf8c07ecef26de603
Elian Hernando Gil Sierra <eliangil@fcv.org>
2024-07-09 12:20:11 -0500

    first commit

---
+++ taskorganizer-backend/src/main/java/eliangilsierra/taskorganizer_backend/TaskorganizerBackendApplication.java
@@ -0,0 +1,13 @@
+package eliangilsierra.taskorganizer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskorganizerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskorganizerBackendApplication.class, args);
+	}
+
+}
---
+++ taskorganizer-backend/src/test/java/eliangilsierra/taskorganizer_backend/TaskorganizerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eliangilsierra.taskorganizer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskorganizerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
